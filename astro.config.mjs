387b10ea139dd674491d7f20b2f9c91afb17a134
Thiago Granata <granatathiago@gmail.com>
2023-12-10 18:37:25 -0300

    fix: added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), svelte()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: vercel()
 });
