561af608b272dbacc4d48c42ca6ceb5b05bddd70
Thiago Granata <granatathiago@gmail.com>
2023-12-11 12:12:16 -0300

    feat: added @astro/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), svelte()],
+  integrations: [tailwind(), mdx(), svelte(), sitemap()],
   output: 'hybrid',
   adapter: vercel(),
-  site: 'https://tmgranata.com',
+  site: 'https://tmgranata.com'
 });
