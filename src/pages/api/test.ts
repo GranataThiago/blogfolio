aac8faa7e7bdb7422fb6026ade1b042e09f0ffb2
Thiago <granatathiago@gmail.com>
2023-12-15 16:13:35 -0300

    test: api route test endpoint

---
+++ src/pages/api/test.ts
@@ -0,0 +1,8 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+    return new Response(
+        JSON.stringify({ message: 'hola' }),
+        { status: 200 }
+    )
+  };
