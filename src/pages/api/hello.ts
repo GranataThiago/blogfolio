484fb0ec3eb59aecd84a02957488ac8ada2bb1ca
Thiago <granatathiago@gmail.com>
2023-12-15 16:23:28 -0300

    fix: content-type

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ request }) => {
-    return new Response(
-        JSON.stringify({ message: 'hola' }),
-        { status: 200 }
-    )
+    return new Response(JSON.stringify({ message: 'hola' }), { 
+        status: 200, 
+        headers: { "Content-Type": "application/json" } 
+    })
   };
