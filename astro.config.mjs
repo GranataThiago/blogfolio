c77bccd52da119626d08c3d89a55710dfdd6c67c
Thiago <granatathiago@gmail.com>
2023-12-26 16:11:11 -0300

    fix: netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), svelte(), sitemap()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   site: 'https://tmgranata.com'
 });
