77df2022504b9e79d2d1c5ebad4e3d32e1ab89fc
Thiago <granatathiago@gmail.com>
2023-12-26 16:27:28 -0300

    fix: 'hybrid'

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), svelte(), sitemap()],
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
   site: 'https://tmgranata.com'
 });
