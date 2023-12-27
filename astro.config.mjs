7f1f62f576a2413c7929b093baa3b3dda467db06
Thiago <granatathiago@gmail.com>
2023-12-27 10:05:26 -0300

    fix: revert to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import svelte from "@astrojs/svelte";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), svelte(), sitemap()],
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
   site: 'https://tmgranata.com'
 });
