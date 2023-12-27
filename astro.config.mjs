b638e75d798ce46e0f55eb800cfc5f936b8f8d15
Thiago <granatathiago@gmail.com>
2023-12-27 09:55:11 -0300

    fix: output server and serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import svelte from "@astrojs/svelte";
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
