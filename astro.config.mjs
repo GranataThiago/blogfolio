5c3daad88d5d8e02f44d2a7ad2c70f30e2796b17
Thiago <granatathiago@gmail.com>
2023-12-27 09:51:44 -0300

    fix: replaced serverless adapter for edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/edge';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), svelte(), sitemap()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   site: 'https://tmgranata.com'
 });
