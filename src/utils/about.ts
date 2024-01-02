f5e88b6961a18c0af4468a42eb1a1cba5e1b8399
Thiago <granatathiago@gmail.com>
2024-01-02 13:32:49 -0300

    refactor

---
+++ src/utils/about.ts
@@ -0,0 +1,14 @@
+export const WORK_EXPERIENCE = [
+    {
+        company: 'Heurix',
+        role: "Fullstack Developer",
+        date: { from: "2021/11", to: "2023/01" }, 
+        technologies: "Angular, Node, Express, Typescript, MySQL, MongoDB, AWS"
+    },
+    {
+        company: 'Heurix',
+        role: "Fullstack Developer",
+        date: { from: "2022/11", to: "2023/01" }, 
+        technologies: "React, Typescript, Postgre, Nest"
+    }
+]
