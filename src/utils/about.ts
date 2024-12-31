e85de7f7183d7aa3e072ad630db51c736f86dadb
tmgranata <granatathiago@gmail.com>
2024-12-31 16:04:18 -0300

    feat: updated work experience

--- src/utils/about.ts
+++ src/utils/about.ts
@@ -10,5 +10,17 @@ export const WORK_EXPERIENCE = [
         role: "Fullstack Developer",
         date: { from: "2022-11-01", to: "2023-01-30" }, 
         technologies: "React, Typescript, Postgre, Nest"
+    },
+    {
+        company: 'Freelancer',
+        role: "Developer",
+        date: { from: "2023-02-01", to: "2024-07-30" }, 
+        technologies: "Node, Typescript, Shopify, "
+    },
+    {
+        company: 'Freelancer',
+        role: "Front-end Developer",
+        date: { from: "2024-08-01", to: "Actualidad" }, 
+        technologies: "Next, Node, Express"
     }
 ]
